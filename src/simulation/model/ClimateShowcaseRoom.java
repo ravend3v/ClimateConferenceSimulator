87581435a4171c85d120fa41ca9c5a3e35a7174d
Alessa Pentinmikko <118171088+alessaem@users.noreply.github.com>
2025-02-12 19:48:01 +0200

    update servicepoints, cause issues

--- src/simulation/model/ClimateShowcaseRoom.java
+++ src/simulation/model/ClimateShowcaseRoom.java
@@ -3,8 +3,15 @@ package simulation.model;
 import simulation.framework.*;
 import eduni.distributions.ContinuousGenerator;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 
 public class ClimateShowcaseRoom extends ServicePoint {
+    private final Set<Customer> processing = new HashSet<>();
+    private Map<Double,Customer> exitTimes = new HashMap<>();
 
     public ClimateShowcaseRoom(ContinuousGenerator generator, EventList eventList, EventType type, int capacity, int currentCustomerCount) {
         super(generator, eventList, type, capacity, currentCustomerCount);
@@ -22,22 +29,56 @@ public class ClimateShowcaseRoom extends ServicePoint {
 
     @Override
     public Customer removeFromQueue() {
-        setBusy(false);
-        currentCustomerCount--;
-        return getQueue().poll();
+        Customer c = null;
+        double smallestTime = Double.MAX_VALUE;
+
+        for(Map.Entry<Double,Customer>entry: exitTimes.entrySet()){
+            if(entry.getKey()<smallestTime){
+                smallestTime = entry.getKey();
+                c = entry.getValue();
+            }
+        }
+
+        if(c!=null){
+            exitTimes.remove(smallestTime);
+            getQueue().remove(c);
+            processing.remove(c);
+            setBusy(false);
+            currentCustomerCount--;
+        }
+
+        return c;
     }
 
     @Override
     public void startService() {
-        if (!isBusy() && getQueue().peek() != null) {
-            Customer customer = getQueue().peek();
-            if (customer != null) {
-                Trace.out(Trace.Level.INFO, "Starting Climate Showcase Room for customer " + customer.getId());
-                double serviceTime = getGenerator().sample();
-                getEventList().add(new Event(getScheduledEventType(), Clock.getInstance().getTime() + serviceTime));
-                currentCustomerCount++;
+        while (currentCustomerCount < capacity && !getQueue().isEmpty()) {
+            Customer customer = getQueue().poll();
+
+            // Check if the customer is null or already being processed
+            if (customer == null || processing.contains(customer)) {
+                continue;
+            }
+            processing.add(customer);
+            Trace.out(Trace.Level.INFO, "Starting climate showcase room for customer " + customer.getId());
+
+            double serviceTime = getGenerator().sample();
+            getEventList().add(new Event(getScheduledEventType(), Clock.getInstance().getTime() + serviceTime));
+            exitTimes.put(Clock.getInstance().getTime() + serviceTime,customer);
+            currentCustomerCount++;
+            System.out.println("current count at climate showcase room: " + currentCustomerCount);
+
+            if (currentCustomerCount == capacity) {
+                Trace.out(Trace.Level.INFO, "climate showcase room is full.");
                 setBusy(true);
+                break;
             }
         }
+
+        for (Customer c : processing) {
+            this.addToQueue(c);
+        }
+
+
     }
 }
